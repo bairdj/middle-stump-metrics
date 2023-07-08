9bc238b121be2f3b0af12c9878d5dd1668f6cc59
James Baird <j.baird94@gmail.com>
2023-07-08 16:48:02 +0100

    Throw error if DB_LOCATION missing

--- R/model.R
+++ R/model.R
@@ -4,6 +4,10 @@ Model <- R6::R6Class(
     connection = NULL,
     initialize = function(path = NULL) {
       if (is.null(path)) {
+        # Throw error if DB_LOCATION is not set
+        if (nchar(Sys.getenv("DB_LOCATION")) == 0) {
+          stop("DB_LOCATION environment variable not set")
+        }
         path <- Sys.getenv("DB_LOCATION")
       }
       self$connection <- DBI::dbConnect(RSQLite::SQLite(), path)
